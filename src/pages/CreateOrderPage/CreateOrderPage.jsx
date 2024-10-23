6fdc73f96258b95de4575b1c50db0ccf01335fc0
Janine Holanda <janine.holanda@gmail.com>
2024-10-23 00:15:10 -0400

    Add defaultValues and handleSubmit on create order page

--- src/pages/CreateOrderPage/CreateOrderPage.jsx
+++ src/pages/CreateOrderPage/CreateOrderPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import data from "../../data/order.json";
 import { Button } from "@/components/ui/button";
 import {
   Accordion,
@@ -7,6 +8,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { formatTime, todayDate } from "../../utils/functions";
 import BeveragesForm from "@/components/BeveragesForm/BeveragesForm";
 import FoodForm from "@/components/FoodForm/ FoodForm";
 import EventDetailsForm from "@/components/EventDetailsForm/EventDetailsForm";
@@ -21,16 +23,52 @@ export default function CreateOrderPage({ isEventHost }) {
     }
   }, [isEventHost, navigate]);
 
+  const defaultValues = {
+    event_name: "",
+    host_name: "",
+    location: "",
+    number_of_guests: "",
+    event_date: todayDate(),
+    event_start_time: formatTime("6:00"),
+    event_end_time: formatTime("6:30"),
+    isBreakfast: true,
+    isAmBreak: false,
+    isLunch: false,
+    isPmBreak: false,
+  };
+
+  const [order, setOrder] = useState(defaultValues);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newOrder = {
+      order_id: data.length + 1,
+      event_name: order.event_name,
+      host_name: order.host_name,
+      location: order.location,
+      number_of_guests: order.number_of_guests,
+      event_date: order.event_date,
+      event_start_time: order.event_start_time,
+      event_end_time: order.event_end_time,
+      isBreakfast: order.isBreakfast,
+      isAmBreak: order.isAmBreak,
+      isLunch: order.isLunch,
+      isPmBreak: order.isPmBreak,
+    };
+    data.push(newOrder);
+    console.log(data);
+  };
+
   if (isEventHost) {
     return (
-      <form action="" className="p-4 bg-slate-100">
-        <Accordion type="single" collapsible className="w-full">
+      <form onSubmit={handleSubmit} className="p-4 bg-slate-100">
+        <Accordion type="single" collapsible className="w-auto">
           <AccordionItem value="eventDetails">
             <AccordionTrigger className="text-2xl mb-4">
               Event Details
             </AccordionTrigger>
             <AccordionContent>
-              <EventDetailsForm />
+              <EventDetailsForm setOrder={setOrder} order={order} />
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="serviceDetails">
@@ -38,11 +76,15 @@ export default function CreateOrderPage({ isEventHost }) {
               Event Service
             </AccordionTrigger>
             <AccordionContent>
-              <EventServiceForm />
+              <EventServiceForm setOrder={setOrder} order={order} />
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="Breakfast">
-            <AccordionTrigger className="text-2xl mb-4">
+            <AccordionTrigger
+              className={`text-2xl mb-4 ${
+                order.isBreakfast ? "visible" : "invisible"
+              }`}
+            >
               Breakfast
             </AccordionTrigger>
             <AccordionContent>
@@ -51,7 +93,11 @@ export default function CreateOrderPage({ isEventHost }) {
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="Am Break">
-            <AccordionTrigger className="text-2xl mb-4">
+            <AccordionTrigger
+              className={`text-2xl mb-4 ${
+                order.isAmBreak ? "visible" : "invisible"
+              }`}
+            >
               Am Break
             </AccordionTrigger>
             <AccordionContent>
@@ -60,14 +106,24 @@ export default function CreateOrderPage({ isEventHost }) {
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="Lunch">
-            <AccordionTrigger className="text-2xl mb-4">Lunch</AccordionTrigger>
+            <AccordionTrigger
+              className={`text-2xl mb-4 ${
+                order.isLunch ? "visible" : "invisible"
+              }`}
+            >
+              Lunch
+            </AccordionTrigger>
             <AccordionContent>
               <BeveragesForm />
               <FoodForm service="Lunch" />
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="Pm Break">
-            <AccordionTrigger className="text-2xl mb-4">
+            <AccordionTrigger
+              className={`text-2xl mb-4 ${
+                order.isPmBreak ? "visible" : "invisible"
+              }`}
+            >
               Pm Break
             </AccordionTrigger>
             <AccordionContent>
@@ -76,6 +132,7 @@ export default function CreateOrderPage({ isEventHost }) {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
+        <Button type="submit">Create an Order</Button>
       </form>
     );
   }
