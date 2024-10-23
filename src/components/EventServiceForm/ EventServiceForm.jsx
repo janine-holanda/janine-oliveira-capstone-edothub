d7c349f491079aecc8de678f4415fc8625cb0184
Janine Holanda <janine.holanda@gmail.com>
2024-10-23 00:11:55 -0400

    Handle changes on inputs from Event Services Form Section

--- src/components/EventServiceForm/ EventServiceForm.jsx
+++ src/components/EventServiceForm/ EventServiceForm.jsx
@@ -1,43 +1,83 @@
-import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
 
-export default function EventServiceForm() {
+export default function EventServiceForm({ order, setOrder }) {
+  const handleChangeIsBreakfast = (event) => {
+    setOrder({ ...order, isBreakfast: event.target.checked });
+  };
+  const handleChangeIsAmBreak = (event) => {
+    setOrder({ ...order, isAmBreak: event.target.checked });
+  };
+  const handleChangeIsLunch = (event) => {
+    setOrder({ ...order, isLunch: event.target.checked });
+  };
+  const handleChangeIsPmBreak = (event) => {
+    setOrder({ ...order, isPmBreak: event.target.checked });
+  };
   return (
     <section className="grid grid-cols-4 gap-4">
       <div className="flex items-center space-x-2">
-        <Checkbox id="breakfast" />
-        <label
-          htmlFor="breakfast"
+        <input
+          id="isBreakfast"
+          type="checkbox"
+          name="isBreakfast"
+          className="checkbox"
+          checked={order.isBreakfast}
+          onChange={handleChangeIsBreakfast}
+        ></input>
+        <Label
+          htmlFor="isBreakfast"
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
         >
           Breakfast
-        </label>
+        </Label>
       </div>
       <div className="flex items-center space-x-2">
-        <Checkbox id="amBreak" />
-        <label
-          htmlFor="amBreak"
+        <input
+          id="isAmBreak"
+          type="checkbox"
+          name="isAmBreak"
+          className="checkbox"
+          checked={order.isAmBreak}
+          onChange={handleChangeIsAmBreak}
+        ></input>
+        <Label
+          htmlFor="isAmBreak"
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
         >
           Am Break
-        </label>
+        </Label>
       </div>
       <div className="flex items-center space-x-2">
-        <Checkbox id="lunch" />
-        <label
-          htmlFor="lunch"
+        <input
+          id="isLunch"
+          type="checkbox"
+          name="isLunch"
+          className="checkbox"
+          checked={order.isLunch}
+          onChange={handleChangeIsLunch}
+        ></input>
+        <Label
+          htmlFor="isLunch"
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
         >
           Lunch
-        </label>
+        </Label>
       </div>
       <div className="flex items-center space-x-2">
-        <Checkbox id="pmBreak" />
-        <label
-          htmlFor="pmBreak"
+        <input
+          id="isPmBreak"
+          type="checkbox"
+          name="isPmBreak"
+          className="checkbox"
+          checked={order.isPmBreak}
+          onChange={handleChangeIsPmBreak}
+        ></input>
+        <Label
+          htmlFor="isPmBreak"
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
         >
           Pm Break
-        </label>
+        </Label>
       </div>
     </section>
   );
