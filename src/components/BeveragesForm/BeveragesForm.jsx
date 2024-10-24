7e915bc933cd145a304135907c75a320448b5e0e
Janine Holanda <janine.holanda@gmail.com>
2024-10-23 20:45:42 -0400

    Add functionality to beverages section for the form

--- src/components/BeveragesForm/BeveragesForm.jsx
+++ src/components/BeveragesForm/BeveragesForm.jsx
@@ -1,24 +1,41 @@
-import { Checkbox } from "@/components/ui/checkbox";
 import menuItems from "../../data/menu-items.json";
+import { Label } from "@/components/ui/label";
 
-export default function BeveragesForm() {
+export default function BeveragesForm({ order, setOrder, isFormEditDisabled }) {
   const beverageMenu = menuItems.find((item) => item.category === "Beverages");
   const beverageMenuItems = beverageMenu.menu;
+  const handleChange = (event, itemName) => {
+    setOrder({
+      ...order,
+      beverage_options: {
+        ...order.beverage_options,
+        [itemName]: event.target.checked,
+      },
+    });
+  };
 
   return (
     <section className="mb-4">
       <h2 className="text-center underline mb-4">Beverages</h2>
       <div className="grid grid-cols-4 gap-4">
-        {beverageMenuItems.map((item) => {
+        {beverageMenuItems.map((menuItem) => {
           return (
-            <div key={item.id} className="flex items-center space-x-2">
-              <Checkbox id={item.item} />
-              <label
-                htmlFor={item.item}
+            <div key={menuItem.id} className="flex items-center space-x-2">
+              <input
+                id={menuItem.item_name}
+                type="checkbox"
+                name={menuItem.item_name}
+                className="checkbox"
+                checked={order.beverage_options[menuItem.item_name]}
+                onChange={(e) => handleChange(e, menuItem.item_name)}
+                disabled={isFormEditDisabled}
+              ></input>
+              <Label
+                htmlFor={menuItem.item_name}
                 className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
               >
-                {item.item}
-              </label>
+                {menuItem.item_description}
+              </Label>
             </div>
           );
         })}
