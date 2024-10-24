badfd254634e6326acf2836483e6fca046439131
Janine Holanda <janine.holanda@gmail.com>
2024-10-23 20:49:44 -0400

    Add disabled input property to Event Details Section in case the form is in edit mode

--- src/components/EventDetailsForm/EventDetailsForm.jsx
+++ src/components/EventDetailsForm/EventDetailsForm.jsx
@@ -1,6 +1,10 @@
 import { Label } from "@/components/ui/label";
 import { formatTime, todayDate } from "../../utils/functions";
-export default function EventDetailsForm({ order, setOrder }) {
+export default function EventDetailsForm({
+  order,
+  setOrder,
+  isFormEditDisabled,
+}) {
   const handleChangeEventName = (event) => {
     setOrder({ ...order, event_name: event.target.value.trimStart() });
   };
@@ -26,7 +30,7 @@ export default function EventDetailsForm({ order, setOrder }) {
   return (
     <section className="grid grid-cols-2 gap-4">
       <div className="grid w-auto col-span-2">
-        <div className="grid w-auto items-center gap-1.5 ml-1">
+        <div className="grid w-auto items-center gap-1.5 ml-1 mr-1">
           <Label htmlFor="eventName" className="text-xs text-slate-500">
             Event Name
           </Label>
@@ -36,11 +40,12 @@ export default function EventDetailsForm({ order, setOrder }) {
             className="input"
             onChange={handleChangeEventName}
             value={order.event_name}
+            disabled={isFormEditDisabled}
           ></input>
         </div>
       </div>
       <div className="grid w-auto col-span-2">
-        <div className="grid w-auto items-center gap-1.5 ml-1">
+        <div className="grid w-auto items-center gap-1.5 ml-1 mr-1">
           <Label htmlFor="hostName" className="text-xs text-slate-500">
             Host Name
           </Label>
@@ -50,11 +55,12 @@ export default function EventDetailsForm({ order, setOrder }) {
             className="input"
             onChange={handleChangeHostName}
             value={order.host_name}
+            disabled={isFormEditDisabled}
           ></input>
         </div>
       </div>
       <div className="grid w-auto col-span-2">
-        <div className="grid w-auto items-center gap-1.5 ml-1">
+        <div className="grid w-auto items-center gap-1.5 ml-1 mr-1">
           <Label htmlFor="location" className="text-xs text-slate-500">
             Location (Room)
           </Label>
@@ -64,10 +70,11 @@ export default function EventDetailsForm({ order, setOrder }) {
             className="input"
             onChange={handleChangeLocation}
             value={order.location}
+            disabled={isFormEditDisabled}
           ></input>
         </div>
       </div>
-      <div className="grid w-fit max-w-sm items-center gap-1.5 ml-1">
+      <div className="grid w-fit max-w-sm items-center gap-1.5 ml-1 mr-1">
         <Label htmlFor="numberGuests" className="text-xs text-slate-500">
           Guests
         </Label>
@@ -80,9 +87,10 @@ export default function EventDetailsForm({ order, setOrder }) {
           className="input w-14"
           onChange={handleChangeNumberOfGuests}
           value={order.number_of_guests}
+          disabled={isFormEditDisabled}
         ></input>
       </div>
-      <div className="grid w-fit max-w-sm items-center gap-1.5 mb-4  ml-1">
+      <div className="grid w-fit max-w-sm items-center gap-1.5 mb-4 ml-1 mr-1">
         <Label htmlFor="eventDate" className="text-xs text-slate-500">
           Date
         </Label>
@@ -94,9 +102,10 @@ export default function EventDetailsForm({ order, setOrder }) {
           className="input w-fit"
           onChange={handleChangeEventDate}
           value={order.event_date}
+          disabled={isFormEditDisabled}
         ></input>
       </div>
-      <div className="grid w-fit max-w-sm items-center gap-1.5 mb-4 ml-1">
+      <div className="grid w-fit max-w-sm items-center gap-1.5 mb-4 ml-1 mr-1">
         <Label htmlFor="startTime" className="text-xs text-slate-500">
           Start Time
         </Label>
@@ -108,9 +117,10 @@ export default function EventDetailsForm({ order, setOrder }) {
           className="input w-fit"
           onChange={handleChangeEventStartTime}
           value={order.event_start_time}
+          disabled={isFormEditDisabled}
         ></input>
       </div>
-      <div className="grid w-fit max-w-sm items-center gap-1.5 mb-4  ml-1">
+      <div className="grid w-fit max-w-sm items-center gap-1.5 mb-4 ml-1 mr-1">
         <Label htmlFor="endTime" className="text-xs text-slate-500">
           End Time
         </Label>
@@ -122,6 +132,7 @@ export default function EventDetailsForm({ order, setOrder }) {
           className="input w-fit"
           onChange={handleChangeEventEndTime}
           value={order.event_end_time}
+          disabled={isFormEditDisabled}
         ></input>
       </div>
     </section>
