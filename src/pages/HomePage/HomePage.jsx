154445e0d3b65236d90ffddb5f4628b955aef27c
Janine Holanda <janine.holanda@gmail.com>
2024-10-25 01:11:38 -0400

    Change Home Page style

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import imageLogo from "../../assets/logo/edothub-logo-30px.svg";
-import { PersonIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { useNavigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, Zoom, toast } from "react-toastify";
+import clientIcon from "../../assets/icons/person_24dp_1E293B_FILL0_wght400_GRAD0_opsz24.svg";
+import managementTeamIcon from "../../assets/icons/groups_24dp_1E293B_FILL0_wght400_GRAD0_opsz24.svg";
 
 export default function HomePage({ setIsEventHost }) {
   const navigate = useNavigate();
@@ -26,8 +27,8 @@ export default function HomePage({ setIsEventHost }) {
   };
 
   return (
-    <section className="bg-rose-100 p-4 h-auto">
-      <section className="bg-e-wine p-4 rounded-md">
+    <section className="bg-rose-50 p-4 h-auto">
+      <section className="bg-gradient-to-t from-rose-900 to-e-wine p-4 rounded-md shadow">
         <h1 className="text-rose-50 mt-4 mb-4">Welcome to</h1>
         <div className="flex justify-center items-center">
           <img className="h-32 mb-4" src={imageLogo} alt="E.Hub Logo" />
@@ -44,18 +45,19 @@ export default function HomePage({ setIsEventHost }) {
         </h2>
         <div className="flex justify-between md:justify-center space-x-8">
           <Button
-            className="border-e-sun hover:bg-e-sun"
+            className=" hover:bg-e-sun"
             variant="outline"
             onClick={handleHost}
           >
-            <PersonIcon /> Event Host
+            <img src={clientIcon} alt="Client Icon" /> Event Host
           </Button>
           <Button
-            className="border-e-sun hover:bg-e-sun"
+            className="hover:bg-e-sun"
             variant="outline"
             onClick={handleManager}
           >
-            <PersonIcon /> Event Manager
+            <img src={managementTeamIcon} alt="Management Team Icon" /> Event
+            Manager
           </Button>
         </div>
       </section>
