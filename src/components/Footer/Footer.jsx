25b3f1948c6b434d21b506f108a29bf79b00ed7f
Janine Holanda <janine.holanda@gmail.com>
2024-10-27 02:33:42 -0400

    Build footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center mt-8 mb-4">
+      <p className="text-e-cloud text-xs font-light ">
+        &copy; E.Hub Inc. All Rights Reserved.{" "}
+      </p>
+    </div>
+  );
+}
