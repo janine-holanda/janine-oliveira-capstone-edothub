5a65f8dc480386f26f28d4f61d5903d1b4af3702
Janine Holanda <janine.holanda@gmail.com>
2024-10-27 22:05:47 -0400

    Add functions to format Date

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -8,10 +8,31 @@ export const formatTime = (time) => {
   });
 };
 
+export const formatTimestampToString = (dateTimestamp) => {
+  const newDate = new Date(dateTimestamp).toLocaleString("en-CA", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+  return newDate;
+};
+
+export const formatTimestampToStringLong = (dateTimestamp) => {
+  const newDate = new Date(dateTimestamp).toLocaleString("en-CA", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return newDate;
+};
+
 export const todayDate = () => {
   const today = new Date();
-  const year = today.toLocaleString("en-US", { year: "numeric" });
-  const month = today.toLocaleString("en-US", { month: "numeric" });
-  const day = today.toLocaleString("en-US", { day: "numeric" });
+  const year = today.toLocaleString("en-CA", { year: "numeric" });
+  const month = today.toLocaleString("en-CA", { month: "numeric" });
+  const day = today.toLocaleString("en-CA", { day: "numeric" });
   return [year, month, day].join("-");
 };
