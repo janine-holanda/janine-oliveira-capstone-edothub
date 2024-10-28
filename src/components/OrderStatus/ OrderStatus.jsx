815fca0e92bbf5b67e80eab2f72c83bb6b44b428
Janine Holanda <janine.holanda@gmail.com>
2024-10-27 22:26:19 -0400

    Change order status style

--- src/components/OrderStatus/ OrderStatus.jsx
+++ src/components/OrderStatus/ OrderStatus.jsx
@@ -2,15 +2,15 @@ export default function OrderStatus({ status }) {
   return (
     <div
       className={`py-1 px-4 w-fit rounded-full border-[1px] flex justify-center ${
-        status === "Accepted" && "border-green-600 bg-green-200"
-      } ${status === "New Order" && "border-e-rose bg-rose-200"} ${
-        status === "Modified" && "border-e-sun bg-yellow-200"
-      } ${status === "Declined" && "border-red-600 bg-red-200"} ${
-        status === "Cancelled" && "border-slate-600 bg-slate-200"
+        status === "Accepted" && "border-green-600 bg-green-50"
+      } ${status === "New Order" && "border-e-rose bg-rose-50"} ${
+        status === "Modified" && "border-e-sun bg-yellow-50"
+      } ${status === "Declined" && "border-red-600 bg-red-50"} ${
+        status === "Cancelled" && "border-slate-600 bg-slate-50"
       }`}
     >
       <p
-        className={`p1 font-semibold ${
+        className={`p1 font-medium ${
           status === "Accepted" && "text-green-600"
         } ${status === "New Order" && "text-e-rose"} ${
           status === "Modified" && "text-e-sun"
